8224ec5e438f41e355dd2db31bfa77c63600ae21
Daniel Weinman <namniewd@gmail.com>
2019-02-27 15:38:24 -0500

    protected routes now require valid token before accessing resources

--- index.js
+++ index.js
@@ -20,7 +20,21 @@ server.use(cors({
 
 // global middleware for protecting routes
 const protected = (req, res, next) => {
-    next();
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                // token was tampered with
+                res.status(401).json({ message: 'You been messing with that token???' });
+            } else {
+                req.decodedJwt = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ message: 'You shall not pass' });
+    }
 }
 
 const generateToken = (user) => {
